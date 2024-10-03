4ab5b3c271267bdeb27abbca01ddb9a96b9d34d4
RicardoWebProject <luisrijhn09@gmail.com>
2024-10-02 22:37:40 -0600

    store added

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,7 +1,32 @@
-import { defineStore } from "pinia";
 import bootstrap from "bootstrap/dist/js/bootstrap.min.js";
-export const useAppStore = defineStore("storeId", {
+import { defineStore } from 'pinia'
+import router from '../router/index'
+
+export const useAppStore = defineStore('auth', {
   state: () => ({
     bootstrap,
+    token: null,
+    usuario: null
   }),
+  actions: {
+    guardarToken(token) {
+      this.token = token;
+      localStorage.setItem('token', token);
+    },
+    autoLogin() {
+      let token = localStorage.getItem('token');
+      if (token) {
+        this.token = token;
+        router.push({ name: 'HelloWorld' });
+      } else {
+        router.push({ name: 'Login' });
+      }
+    },
+    salir() {
+      this.token = null;
+      localStorage.removeItem('token');
+      router.push({ name: 'Login' });
+    }
+  }
 });
+
