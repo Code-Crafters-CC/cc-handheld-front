ba31cca4b0562412785da2be7c8c9a784bf0b3df
RicardoWebProject <luisrijhn09@gmail.com>
2024-10-04 23:58:35 -0600

    changed url

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import materialKit from "./material-kit";
 
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+axios.defaults.baseURL = 'https://pg2umg.pythonanywhere.com/'
 
 const app = createApp(App);
 
