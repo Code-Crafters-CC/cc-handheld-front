40076b33ba4609c844ce37f6caa2f8b37d19395b
RicardoWebProject <luisrijhn09@gmail.com>
2025-02-01 20:26:03 -0600

    Edited buttons

--- src/views/LandingPages/AboutUs/Sections/AboutInformation.vue
+++ src/views/LandingPages/AboutUs/Sections/AboutInformation.vue
@@ -1,31 +1,33 @@
 <script setup>
 // example components
 import DefaultInfoCard from "../../../../examples/cards/infoCards/DefaultInfoCard.vue";
+import {useAppStore} from '@/stores/index';
+const appStore = useAppStore();
 </script>
 
 <template>
   <section class="py-7">
     <div class="container">
-      <div class="row align-items-center">
+      <div class="row align-items-center" v-if="appStore.token">
         <div class="col-lg-6">
           <div class="row justify-content-start">
             <DefaultInfoCard
               color="success"
               icon="hail"
-              title="Ingresar jugadores"
+              title="RECEPCIÓN"
               description="Registra y organiza a los jugadores"
             />
             <router-link :to="{ name: '' }">
-              <button type="button" class="btn btn-dark btn-sm" style="width: 400px; height: 50px;">Agregar</button>
+              <button type="button" class="btn btn-dark btn-sm" style="width: 200px; height: 50px;">Agregar</button>
             </router-link>
             <DefaultInfoCard
               color="info"
               icon="people"
-              title="Crear equipo"
+              title="TRASLADO"
               description="Crea y organiza a tus equipos"
             />
             <router-link :to="{ name: '' }">
-              <button type="button" class="btn btn-dark btn-sm" style="width: 400px; height: 50px;">Crear</button>
+              <button type="button" class="btn btn-dark btn-sm" style="width: 200px; height: 50px;">Crear</button>
             </router-link>
           </div>
         </div>
@@ -34,20 +36,20 @@ import DefaultInfoCard from "../../../../examples/cards/infoCards/DefaultInfoCar
             <DefaultInfoCard
               color="info"
               icon="groups"
-              title="Ver Equipos"
+              title="PRODUCTO"
               description="Organiza a tus equipos y a sus jugadores"
             />
             <router-link :to="{ name: '' }">
-              <button type="button" class="btn btn-dark btn-sm" style="width: 400px; height: 50px;">Ver</button>
+              <button type="button" class="btn btn-dark btn-sm" style="width: 200px; height: 50px;">Ver</button>
             </router-link>
             <DefaultInfoCard
               color="info"
               icon="equalizer"
-              title="Ver estadisticas"
+              title="AUDITAR"
               description="Observa las estadisticas de los partidos"
             />
             <router-link :to="{ name: '' }">
-              <button type="button" class="btn btn-dark btn-sm" style="width: 400px; height: 50px;">Estadisicas</button>
+              <button type="button" class="btn btn-dark btn-sm" style="width: 200px; height: 50px;">Estadisicas</button>
             </router-link>
           </div>
         </div>
